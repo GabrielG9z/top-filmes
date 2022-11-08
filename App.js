fb39c1146e093669335ca373abd56a01b91c2a28
Gabriel Genovez <86001324+GabrielG9z@users.noreply.github.com>
2022-11-08 09:27:26 -0300

    Update
    Criados as componentes screens de exibição que serão apresentadas no App.js

--- App.js
+++ App.js
@@ -1,123 +1,15 @@
-import {
-  Button,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  Pressable,
-} from "react-native";
-import React from "react";
-import { useFonts } from "expo-font";
-import logo from "./assets/images/logo.png";
-
-import { Ionicons } from "@expo/vector-icons";
+import { StatusBar } from "react-native";
+import Favoritos from "./src/screens/Favoritos";
+import FormBusca from "./src/screens/FormBusca";
 
 const App = () => {
-  const [fonteCarregada] = useFonts({
-    monoton: require("./assets/fonts/Monoton-Regular.ttf"),
-  });
-
-  /* A condicional abaixo serve apenas para dar um pequeno tempo suficiente para o carregamento do arquivo de fonte antes do return do componente. */
-  if (!fonteCarregada) return <Text>Fonte sendo carregada...</Text>;
-
   return (
-    <SafeAreaView style={estilos.container}>
-      <View style={estilos.viewLogo}>
-        <Image style={estilos.logo} source={logo} />
-        <Text style={estilos.tituloApp}>Top-filmes</Text>
-      </View>
-
-      <View style={estilos.viewBotoes}>
-        <Pressable style={estilos.botaoInicial}>
-          <Text style={estilos.textoBotao}>
-            <Ionicons name="ios-search-outline" size={16} color="white" />{" "}
-            Buscar Filmes
-          </Text>
-        </Pressable>
-
-        <Pressable style={estilos.botaoInicial}>
-          <Text style={estilos.textoBotao}>
-            {" "}
-            Favoritos
-            <Ionicons name="star" size={16} color="white" />
-          </Text>
-        </Pressable>
-      </View>
-
-      <View style={estilos.viewRodape}>
-        <Pressable style={estilos.botaoFooter}>
-          <Text style={estilos.textoBotao}>
-            <Ionicons name="lock-closed" size={16} color="white" /> Privacidade
-          </Text>
-        </Pressable>
-
-        <Pressable style={estilos.botaoFooter}>
-          <Text style={estilos.textoBotao}>
-            <Ionicons
-              name="information-circle-outline"
-              size={16}
-              color="white"
-            />{" "}
-            Sobre
-          </Text>
-        </Pressable>
-      </View>
-    </SafeAreaView>
+    <>
+      {/* Opções para o barStyle: dark-content, light-content ou default */}
+      <StatusBar barStyle="light-content" />
+      <Favoritos />
+    </>
   );
 };
 
 export default App;
-
-const estilos = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  viewLogo: {
-    flex: 3,
-    textAlign: "center",
-    width: "80%",
-    justifyContent: "flex-end",
-    alignItems: "center",
-  },
-  logo: {
-    width: 128,
-    height: 128,
-  },
-  tituloApp: {
-    fontFamily: "monoton",
-    fontSize: 36,
-    color: "#5451a6",
-    // fontWeight: "bold",
-  },
-  viewBotoes: {
-    flex: 2,
-    width: "80%",
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-    alignItems: "flex-start",
-  },
-  viewRodape: {
-    justifyContent: "space-between",
-    flex: 0.5,
-    width: "100%",
-    flexDirection: "row",
-    backgroundColor: "#c465d1",
-  },
-  botaoInicial: {
-    borderRadius: 7,
-    borderStyle: "solid",
-    borderWidth: 1,
-    padding: 16,
-    backgroundColor: "#c465d1",
-    alignContent: "center",
-  },
-  botaoFooter: {
-    padding: 22,
-  },
-  textoBotao: {
-    color: "white",
-  },
-});
---
+++ src/screens/Home.js
@@ -0,0 +1,118 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Pressable,
+  Image,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { useFonts } from "expo-font";
+import logo from "../../assets/images/logo.png";
+
+const Home = () => {
+  const [fonteCarregada] = useFonts({
+    monoton: require("../../assets/fonts/Monoton-Regular.ttf"),
+  });
+
+  /* A condicional abaixo serve apenas para dar um pequeno tempo suficiente para o carregamento do arquivo de fonte antes do return do componente. */
+  if (!fonteCarregada) return <Text>Fonte sendo carregada...</Text>;
+
+  return (
+    <SafeAreaView style={estilos.container}>
+      <View style={estilos.viewLogo}>
+        <Image style={estilos.logo} source={logo} />
+        <Text style={estilos.tituloApp}>Top-filmes</Text>
+      </View>
+
+      <View style={estilos.viewBotoes}>
+        <Pressable style={estilos.botaoInicial}>
+          <Text style={estilos.textoBotao}>
+            <Ionicons name="ios-search-outline" size={16} color="white" />
+            Buscar Filmes
+          </Text>
+        </Pressable>
+
+        <Pressable style={estilos.botaoInicial}>
+          <Text style={estilos.textoBotao}>
+            Favoritos
+            <Ionicons name="star" size={16} color="white" />
+          </Text>
+        </Pressable>
+      </View>
+
+      <View style={estilos.viewRodape}>
+        <Pressable style={estilos.botaoFooter}>
+          <Text style={estilos.textoBotao}>
+            <Ionicons name="lock-closed" size={16} color="white" /> Privacidade
+          </Text>
+        </Pressable>
+
+        <Pressable style={estilos.botaoFooter}>
+          <Text style={estilos.textoBotao}>
+            <Ionicons
+              name="information-circle-outline"
+              size={16}
+              color="white"
+            />
+            Sobre
+          </Text>
+        </Pressable>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
+const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  viewLogo: {
+    flex: 3,
+    textAlign: "center",
+    width: "80%",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  logo: {
+    width: 128,
+    height: 128,
+  },
+  tituloApp: {
+    fontFamily: "monoton",
+    fontSize: 36,
+    color: "#5451a6",
+    // fontWeight: "bold",
+  },
+  viewBotoes: {
+    flex: 2,
+    width: "80%",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "flex-start",
+  },
+  viewRodape: {
+    justifyContent: "space-between",
+    flex: 0.5,
+    width: "100%",
+    flexDirection: "row",
+    backgroundColor: "#c465d1",
+  },
+  botaoInicial: {
+    borderRadius: 7,
+    borderStyle: "solid",
+    borderWidth: 1,
+    padding: 16,
+    backgroundColor: "#c465d1",
+    alignContent: "center",
+  },
+  botaoFooter: {
+    padding: 22,
+  },
+  textoBotao: {
+    color: "white",
+  },
+});
