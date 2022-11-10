5feba21e6c534d8ea69ef94c530c8bf2167a8c33
Gabriel Genovez <86001324+GabrielG9z@users.noreply.github.com>
2022-11-10 11:56:17 -0300

    Update
    Fizemos a conexão com a API e realizamos a query com exibição de resultados com arrow function map

--- src/screens/Resultado.js
+++ src/screens/Resultado.js
@@ -1,12 +1,43 @@
+import { useEffect, useState } from "react";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import api from "../services/api";
 
 const Resultados = ({ route }) => {
   /* Usamos a prop route (do React Navigation) para acessar os parâmetros desta rota de navegação e extrair os dados (neste caso, filme) enviados para esta tela Resultados */
   const { filme } = route.params;
-  console.log(filme);
+
+  const [resultados, setResultados] = useState([]);
+
+  useEffect(() => {
+    async function buscarFilmes() {
+      try {
+        const resposta = await api.get("/search/movie", {
+          params: {
+            api_key: "372a13f141ce4f6618d8a2cbaeecf7ef",
+            language: "pt-BR",
+            query: filme,
+            include_adult: true,
+          },
+        });
+
+        setResultados(resposta.data.results);
+      } catch (error) {
+        console.log("Deu ruim a busca na API:" + error.message);
+      }
+    }
+    buscarFilmes();
+  }, []);
+
+  console.log(resultados);
+
   return (
     <SafeAreaView style={estilos.container}>
       <Text>Você buscou por: {filme}</Text>
+      <View style={estilos.viewFilmes}>
+        {resultados.map((resultado) => {
+          return <Text key={resultado.id}>{resultado.title}</Text>;
+        })}
+      </View>
     </SafeAreaView>
   );
 };
@@ -17,5 +48,8 @@ const estilos = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
+  },
+  viewfIlmes: {
+    marginVertical: 8,
   },
 });
